a1c2a0f5fa65f841e24c4ff9b2c40e399d9c6f8e
emanuelbarajas <barajasemanuel2@gmail.com>
2026-04-21 00:13:09 -0700

    Interfacing With Sensors

---
+++ Assignments/EE 310 Interfacing With Sensors/build/default/debug/main.p1.d
@@ -0,0 +1,5 @@
+build/default/debug/main.p1:  \
+main.c  \
+config.h  \
+init.h  \
+functions.h 
---
+++ Assignments/EE 310 Interfacing With Sensors/functions.h
@@ -0,0 +1,11 @@
+#ifndef FUNCS_H
+#define FUNCS_H
+
+void showDigit(unsigned char digit);
+unsigned char readPR1Digit(void);
+unsigned char readPR2Digit(void);
+void goodCode(void);
+void badCode(void);
+void melodyISR(void);
+
+#endif
